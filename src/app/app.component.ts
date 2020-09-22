eccc430e2fb557567686fc1bfbc5ffbbe652f2fd
luciferRK <likhithr.cs17@sahyadri.edu.in>
2020-09-22 15:25:29 +0530

    added 404

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent{
+    constructor(private router:Router){
+      let path = localStorage.getItem('path');
+      if(path) {
+        localStorage.removeItem('path');
+        this.router.navigate([path]);
+      }
+    }
 }
